0079a88e0e349248adf5387cbd67032f2478be47
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-21 17:16:49 +0100

    consegna completed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const immagineLampadina = document.querySelector('.img-fluid');
+const immagineLampadinaSpenta = document.querySelector('.img-fluid');
+const immagineLampadinaAccesa = document.getElementById('on');
 const bottoneLampadina = document.querySelector('.btn');
 
+
 bottoneLampadina.addEventListener('click', () =>{
-  target.innerHTML =
+  immagineLampadinaAccesa.classList.toggle('hide');
+  immagineLampadinaSpenta.classList.toggle('hide');
 })
