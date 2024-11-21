24014071bec0b1322580ec343e7f63d85f62e5fe
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-21 18:07:52 +0100

    bonus completed + add comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const immagineLampadinaSpenta = document.querySelector('.img-fluid');
-const immagineLampadinaAccesa = document.getElementById('on');
-const bottoneLampadina = document.querySelector('.btn');
+//VARIABILI
+const immagineLampadinaSpenta = document.getElementById('light off');
+const immagineLampadinaAccesa = document.getElementById('light on');
+//VARIABILI
+const bottoneLampadinaSpenta = document.querySelector('.btn');
+const bottoneLampadina = document.getElementById('bottone')
 
-
+//SELEZIONO IL PARAGRAFO CREATO DINAMICAMENTE E UNA VOLTA TROVATO LO INVOCO
 bottoneLampadina.addEventListener('click', () =>{
   immagineLampadinaAccesa.classList.toggle('hide');
+  bottoneLampadina.innerHTML = immagineLampadinaSpenta.classList.contains('hide') ? 'Accendi' : 'Spegni';
   immagineLampadinaSpenta.classList.toggle('hide');
 })
